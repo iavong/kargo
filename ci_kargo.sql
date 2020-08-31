b3eee81e15dfc4639280789be3aa61a43333a4bd
murprakoso <prakoso.clan04@gmail.com>
2020-08-31 22:52:55 +0700

    rancangan database awal

---
+++ ci_kargo.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 31 Agu 2020 pada 17.18
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci_kargo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `harga`
+--
+
+CREATE TABLE `harga` (
+  `id` int(11) NOT NULL,
+  `berat` decimal(10,0) DEFAULT NULL,
+  `biaya` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id` bigint(20) NOT NULL,
+  `keterangan` text DEFAULT NULL,
+  `harga` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerima`
+--
+
+CREATE TABLE `penerima` (
+  `id` bigint(20) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `kota` varchar(100) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengirim`
+--
+
+CREATE TABLE `pengirim` (
+  `id` bigint(20) NOT NULL,
+  `nama` varchar(250) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `kota` varchar(100) DEFAULT NULL,
+  `alamat` varchar(200) DEFAULT NULL,
+  `deposit` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id` bigint(20) NOT NULL,
+  `no_kwitansi` int(11) DEFAULT NULL,
+  `airlines` varchar(100) DEFAULT NULL,
+  `no_penerbangan` varchar(100) DEFAULT NULL,
+  `no_smu` varchar(100) DEFAULT NULL,
+  `berat` decimal(10,0) DEFAULT NULL,
+  `koli` int(11) DEFAULT NULL,
+  `biaya_smu` varchar(100) DEFAULT NULL,
+  `biaya_gudang` varchar(100) DEFAULT NULL,
+  `biaya_tambahan` varchar(100) DEFAULT NULL,
+  `biaya_total` varchar(100) DEFAULT NULL,
+  `isi` text DEFAULT NULL,
+  `catatan` text DEFAULT NULL,
+  `pengirim` varchar(100) DEFAULT NULL,
+  `penerima` varchar(100) DEFAULT NULL,
+  `jenis_pembayaran` enum('deposit','cash','debit') DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `role` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `harga`
+--
+ALTER TABLE `harga`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `penerima`
+--
+ALTER TABLE `penerima`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengirim`
+--
+ALTER TABLE `pengirim`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `harga`
+--
+ALTER TABLE `harga`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penerima`
+--
+ALTER TABLE `penerima`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengirim`
+--
+ALTER TABLE `pengirim`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
