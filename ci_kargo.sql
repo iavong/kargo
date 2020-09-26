4c8d3b0184a0b58e35930eb5bb272e892383e5d8
murprakoso <prakoso.clan04@gmail.com>
2020-09-27 03:07:02 +0700

    export database

--- ci_kargo.sql
+++ ci_kargo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 14 Sep 2020 pada 00.38
+-- Waktu pembuatan: 26 Sep 2020 pada 22.05
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.9
 
@@ -31,6 +31,7 @@ CREATE TABLE `deposit` (
   `id` bigint(20) NOT NULL,
   `id_pengirim` bigint(20) DEFAULT NULL,
   `deposit` varchar(100) DEFAULT NULL,
+  `tipe` tinyint(4) NOT NULL DEFAULT 1 COMMENT '1=In,0=Out',
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -38,10 +39,15 @@ CREATE TABLE `deposit` (
 -- Dumping data untuk tabel `deposit`
 --
 
-INSERT INTO `deposit` (`id`, `id_pengirim`, `deposit`, `created_at`) VALUES
-(23, 8, '200000', '2020-09-13 23:30:53'),
-(24, 9, '1000000', '2020-09-13 23:41:57'),
-(26, 9, '150000', '2020-09-13 23:42:17');
+INSERT INTO `deposit` (`id`, `id_pengirim`, `deposit`, `tipe`, `created_at`) VALUES
+(24, 9, '1000000', 1, '2020-09-13 23:41:57'),
+(26, 9, '150000', 1, '2020-09-13 23:42:17'),
+(28, 9, '20000', 1, '2020-09-26 02:30:42'),
+(29, 9, '75000', 0, '2020-09-26 02:35:41'),
+(33, 8, '20000', 1, '2020-09-26 02:44:52'),
+(34, 8, '180000', 1, '2020-09-26 02:44:57'),
+(35, 8, '70000', 0, '2020-09-26 02:46:18'),
+(36, 8, '320000', 0, '2020-09-26 02:52:55');
 
 -- --------------------------------------------------------
 
@@ -100,8 +106,8 @@ CREATE TABLE `pengirim` (
 --
 
 INSERT INTO `pengirim` (`id`, `nama`, `no_hp`, `alamat`, `deposit`, `created_at`) VALUES
-(8, 'Ahmad', '089999999', 'pontianak', '200000', '2020-09-13 23:30:38'),
-(9, 'Iavong', '0884787485784', 'pontianak', '1150000', '2020-09-13 23:41:45');
+(8, 'Ahmad', '089999999', 'pontianak', '-190000', '2020-09-13 23:30:38'),
+(9, 'Iavong', '0884787485784', 'pontianak', '1095000', '2020-09-13 23:41:45');
 
 -- --------------------------------------------------------
 
@@ -137,7 +143,10 @@ CREATE TABLE `penjualan` (
 INSERT INTO `penjualan` (`id`, `no_kwitansi`, `airlines`, `no_penerbangan`, `no_smu`, `berat`, `koli`, `biaya_smu`, `biaya_gudang`, `biaya_tambahan`, `biaya_total`, `isi`, `catatan`, `pengirim`, `penerima`, `tujuan_id`, `jenis_pembayaran`, `created_at`) VALUES
 (4, 0, 'lion', 'lakdjkf', 'alksdjf', '10', 2, '200000', '150000', '10000', '360000', '', '', 'Didi', 'baban', 15, 'deposit', '2020-09-14 04:11:45'),
 (5, 0, 'JT', 'JT', 'lakjsdf', '5', 1, '100000', '50000', '', '150000', '', '', 'jinton', 'naruto', 15, 'deposit', '2020-09-14 04:12:54'),
-(6, 0, 'lion', 'lakjdf', 'akldjf', '10', 2, '100000', '15000', '10000', '125000', '', '', 'alksdjf', 'aksdjf', 15, 'deposit', '2020-09-14 04:45:11');
+(6, 0, 'lion', 'lakjdf', 'akldjf', '10', 2, '100000', '15000', '10000', '125000', '', '', 'alksdjf', 'aksdjf', 15, 'deposit', '2020-09-14 04:45:11'),
+(7, 0, 'lion', 'sabeb', '23', '5', 3, '50000', '20000', '5000', '75000', '', '', 'Iavong', 'Sembarang', 15, 'deposit', '2020-09-26 02:18:31'),
+(9, 0, 'lion', '45', '45', '5', 2, '50000', '20000', '', '70000', '', '', 'Ahmad', 'degi', 15, 'deposit', '2020-09-26 02:46:18'),
+(10, 0, '45', '45', '45', '10', 2, '300000', '20000', '', '320000', '', '', 'Ahmad', 'degi', 14, 'deposit', '2020-09-26 02:52:55');
 
 -- --------------------------------------------------------
 
@@ -244,7 +253,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `deposit`
 --
 ALTER TABLE `deposit`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT untuk tabel `pembelian`
@@ -268,7 +277,7 @@ ALTER TABLE `pengirim`
 -- AUTO_INCREMENT untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `tujuan`
